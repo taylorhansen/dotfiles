dbbf499e60fbc82cdd82dea3730d6f33f350e9d9
CrazyGuy108 <taylor@hansenfamily.us>
2019-01-24 19:19:58 -0800

    Move aliases to a separate file
    The g=git alias also wasn't getting tab-autocomplete so fix that using a
    builtin git command.

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -8,7 +8,7 @@ mkdir -p $olddir
 
 # make all those symlinks
 dir=~/dotfiles
-files='bashrc vimrc gitconfig'
+files='bashrc bash_aliases vimrc gitconfig'
 echo 'Creating symlinks for:'
 cd $dir
 for file in $files
