84de85e446b3ac3cbe6ea3f849d24329a4b3f202
taylorhansen <taylor@hansenfamily.us>
2020-12-25 17:20:19 -0800

    Add neovim init.vim
    Requires modifying make-symlinks.sh to not prepend the name with a dot.

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -9,35 +9,38 @@ mkdir -p $olddir
 # make all those symlinks
 dir=~/dotfiles
 typeset -A file_locs=( \
-[bashrc]=~ \
-[bash_aliases]=~ \
-[gitconfig]=~ \
-[nethackrc]=~ \
-[npmrc]=~ \
-[vimrc]=~ )
+[bashrc]=~/.bashrc \
+[bash_aliases]=~/.bash_aliases \
+[gitconfig]=~/.gitconfig \
+[init.vim]=~/.config/nvim/init.vim \
+[nethackrc]=~/.nethackrc \
+[npmrc]=~/.npmrc \
+[vimrc]=~/.vimrc )
 
 echo 'Creating symlinks for:'
 cd $dir
 for file in ${!file_locs[@]}
 do
-    loc=${file_locs[$file]}
+    dotfile=${file_locs[$file]}
+    loc=$(dirname $dotfile)
 
     # make sure the directory exists
     if [ -d $loc ]
     then
-        echo "- .$file in $loc"
+        echo "- $file in $loc"
 
         # move old dotfile to backup folder
-        if [ -f $loc/.$file ]
+        if [ -f $loc/$dotfile ]
         then
-            mv $loc/.$file $olddir/.$file
+            mv $loc/$dotfile $olddir/$file
         fi
 
         # link dotfile location to this repository's dotfile
-        ln -sf $dir/$file $loc/.$file
+        ln -sf $dir/$file $dotfile
     else
         # $loc doesn't exist
-        echo "- .$file in $loc (error: doesn't exist)"
+        echo "- $file in $loc (error: doesn't exist)"
     fi
 done
+
 echo 'Done making symlinks'
